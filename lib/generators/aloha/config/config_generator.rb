db5805edec2f8e92412c7b9d86e49a2e5a7e840f
Gudleik Rasch <gudleik@gmail.com>
2012-01-30 22:27:44 +0100

    added option to install .js file

--- lib/generators/aloha/config/config_generator.rb
+++ lib/generators/aloha/config/config_generator.rb
@@ -1,7 +1,12 @@
 class Aloha::ConfigGenerator < Rails::Generators::Base
   source_root File.expand_path('../templates', __FILE__)
+  class_option :js, :type => :boolean, :default => false, :desc => "Install .js file instead of coffeescript"
 
-  def copy_coffee
-    template 'aloha-config.coffee', File.join('app', 'assets', 'javascripts', 'aloha-config.coffee')
+  def copy_asset
+    if options[:js]
+      template 'aloha-config.js', File.join('app', 'assets', 'javascripts', 'aloha-config.js')
+    else
+      template 'aloha-config.coffee', File.join('app', 'assets', 'javascripts', 'aloha-config.coffee')
+    end
   end
 end
