a955e1d95c03438c0d2d0069ddbcd3500a6f1228
Gudleik Rasch <gudleik@gmail.com>
2012-01-31 09:38:52 +0100

    default plugins and inject aloha-config to application.js

--- lib/aloha-rails.rb
+++ lib/aloha-rails.rb
@@ -8,7 +8,6 @@ module Aloha
     autoload :Helpers, 'aloha-rails/helpers'
 
     mattr_accessor :default_plugins
-
-    self.default_plugins = ['common/format', 'common/list', 'common/link']
+    self.default_plugins = %w(common/format common/list common/link common/block common/undo common/contenthandler common/paste)
   end
 end
--- lib/generators/aloha/install/install_generator.rb
+++ lib/generators/aloha/install/install_generator.rb
@@ -1,16 +1,25 @@
 class Aloha::InstallGenerator < Rails::Generators::Base
-  source_root File.expand_path('../templates', __FILE__)
+  source_root File.expand_path("../templates", __FILE__)
   class_option :js, :type => :boolean, :default => false, :desc => "Install .js file instead of coffeescript"
 
   def copy_asset
     if options[:js]
-      template 'aloha-config.js', File.join('app', 'assets', 'javascripts', 'aloha-config.js')
+      template "aloha-config.js", File.join("app", "assets", "javascripts", "aloha-config.js")
     else
-      template 'aloha-config.coffee', File.join('app', 'assets', 'javascripts', 'aloha-config.coffee')
+      template "aloha-config.coffee", File.join("app", "assets", "javascripts", "aloha-config.coffee")
     end
   end
 
   def copy_initializer
-    template "aloha.rb", File.join('config', 'initializers', 'aloha.rb')
+    template "aloha.rb", File.join("config", "initializers", "aloha.rb")
+  end
+
+  def load_javascript
+    appjs = File.join "app", "assets", "javascripts", "application.js"
+    if File.exist? appjs
+      insert_into_file appjs, "//= require aloha-config\n", :after => "jquery_ujs\n"
+    else
+      copy_file "application.js", appjs
+    end
   end
 end
