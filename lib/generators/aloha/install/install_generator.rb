0fb2a82f544578f56d409269e2cd42e19aee2af1
Gudleik Rasch <gudleik@gmail.com>
2012-01-31 01:00:32 +0100

    renamed the config generator to install

--- lib/generators/aloha/install/install_generator.rb
+++ lib/generators/aloha/install/install_generator.rb
@@ -1,4 +1,4 @@
-class Aloha::ConfigGenerator < Rails::Generators::Base
+class Aloha::InstallGenerator < Rails::Generators::Base
   source_root File.expand_path('../templates', __FILE__)
   class_option :js, :type => :boolean, :default => false, :desc => "Install .js file instead of coffeescript"
 
@@ -8,5 +8,9 @@ class Aloha::ConfigGenerator < Rails::Generators::Base
     else
       template 'aloha-config.coffee', File.join('app', 'assets', 'javascripts', 'aloha-config.coffee')
     end
+  end
+
+  def copy_initializer
+    template "aloha.rb", File.join('config', 'initializers', 'aloha.rb')
   end
 end
---
+++ lib/generators/aloha/install/templates/aloha.rb
@@ -0,0 +1,3 @@
+# Aloha configuration
+
+Aloha::Rails.default_plugins = %w(common/format common/list common/link common/block common/undo common/contenthandler common/paste)
